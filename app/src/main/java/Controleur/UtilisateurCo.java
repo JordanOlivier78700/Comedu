0b1e32eeeafcd296a51d6c071bc26280b56f763f
JordanOlivier78700 <34971000+JordanOlivier78700@users.noreply.github.com>
2018-07-01 16:49:17 +0200

    Regu layouts

--- app/src/main/java/Controleur/UtilisateurCo.java
+++ app/src/main/java/Controleur/UtilisateurCo.java
@@ -3,17 +3,20 @@ package Controleur;
 public class UtilisateurCo {
 
   public String login;
-
   public String password;
-
   public String nom;
-
   public String prenom;
 
-  public void seConnecter() {
+  public void seConnecter()
+  {
+    //if(utilisateurDAO.recherche("login","password"))
+      //je me co
+    //else
+      //faux pass ou compte
   }
 
-  public void seDeconnecter() {
+  public void seDeconnecter()
+  {
   }
 
 }
