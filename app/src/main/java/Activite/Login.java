2ef48061d6deb53758fe70a4eaf7213f229e3c8e
JordanOlivier78700 <34971000+JordanOlivier78700@users.noreply.github.com>
2018-07-03 16:11:09 +0200

    prepa pour DOA

--- app/src/main/java/Activite/Login.java
+++ app/src/main/java/Activite/Login.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.jogral.comedu.R;
 
 public class Login extends AppCompatActivity {
+    EditText login;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        login = (EditText) findViewById (R.id.et_login);
+        password = (EditText) findViewById (R.id.et_password);
     }
 
     public void click_quitter(View view)
@@ -21,10 +26,18 @@ public class Login extends AppCompatActivity {
     }
     public void click_valider(View view)
     {
-        //Connection bd
-        //if
-        // création d'un session
-        startActivity(new Intent(this, Home_connecter_admin.class));
-        //else message derreur
+        /*
+        String str_Login = login.getText().toString();
+        String str_Password = password.getText().toString();
+        if (!str_Login.isEmpty())
+        {
+            DAO dao_login = new DAO();
+            boolean bool = dao_login.recherche(str_Login, str_Password);
+            if (bool) {
+                startActivity(new Intent(this, Home_connecter_admin.class));
+            } else {
+                //msg erreur
+            }
+        }*/
     }
 }
