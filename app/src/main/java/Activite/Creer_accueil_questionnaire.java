e687661da827c444b86f725bc83140a0521d9995
JordanOlivier78700 <34971000+JordanOlivier78700@users.noreply.github.com>
2018-07-03 09:55:17 +0200

    Activités + Fonction click
    Ajout des activités correspondant aux layouts.
    Ajout des fonctions des dans les activités pour les clicks

--- app/src/main/java/Activite/Creer_accueil_questionnaire.java
+++ app/src/main/java/Activite/Creer_accueil_questionnaire.java
@@ -7,24 +7,21 @@ import android.view.View;
 
 import com.example.jogral.comedu.R;
 
-public class Login extends AppCompatActivity {
+public class Creer_accueil_questionnaire extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_layout);
-    }
-
-    public void click_quitter(View view)
-    {
-        startActivity(new Intent(this, Home.class));
+        setContentView(R.layout.creer_accueil_questionnaire_layout);
     }
     public void click_valider(View view)
     {
-        //Connection bd
-        //if
-        // création d'un session
+        //Création d'un objet de type questionnaire
         startActivity(new Intent(this, Home_connecter_admin.class));
-        //else message derreur
+
+    }
+    public void click_quitter(View view)
+    {
+        startActivity(new Intent(this, Home_connecter_admin.class));
     }
 }
--- app/src/main/java/Activite/Visiteur.java
+++ app/src/main/java/Activite/Visiteur.java
@@ -1,26 +1,17 @@
 package Activite;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
 import com.example.jogral.comedu.R;
 
-public class Home extends AppCompatActivity {
+public class Visiteur extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
-    }
-
-    public void click_co(View view)
-    {
-        startActivity(new Intent(this, Login.class));
-    }
-
-    public void click_visiteur(View view)
-    {
-        startActivity(new Intent(this, Login.class));
     }
 }
