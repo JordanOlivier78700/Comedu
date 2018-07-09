5f078a9bf966952fb99e4cfae5692658deacd60c
JordanOlivier78700 <34971000+JordanOlivier78700@users.noreply.github.com>
2018-07-09 10:18:55 +0200

    Definition des classes controleur
    Suppression des DAO inutiles
    Complétion des classes controleur

--- app/src/main/java/Controleur/UtilisateurCo.java
+++ app/src/main/java/Controleur/UtilisateurCo.java
@@ -1,16 +1,53 @@
 package Controleur;
 
-public class UtilisateurCo {
+public class UtilisateurCo
+{
 
-  public String login;
+  private String login;
+  private boolean adm;
+  private String prenom;
+  private String nom;
+  private String classe;
 
+  public String getLogin() {
+    return login;
+  }
 
+  public void setLogin(String login) {
+    this.login = login;
+  }
 
-  public String password;
+  public boolean isAdm() {
+    return adm;
+  }
 
-  public String nom;
+  public void setAdm(boolean adm) {
+    this.adm = adm;
+  }
 
-  public String prenom;
+  public String getPrenom() {
+    return prenom;
+  }
+
+  public void setPrenom(String prenom) {
+    this.prenom = prenom;
+  }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public String getClasse() {
+    return classe;
+  }
+
+  public void setClasse(String classe) {
+    this.classe = classe;
+  }
 
   public void seConnecter() {
   }
