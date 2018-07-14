7174fd43591ba40f2a2940623c285a6eefd68bc6
JordanOlivier78700 <jordan.olivier78700@gmail.com>
2018-07-14 20:49:59 +0200

    envoie d'un obect de q1 a q2

--- app/src/main/java/Controleur/Resultat.java
+++ app/src/main/java/Controleur/Resultat.java
@@ -1,7 +1,17 @@
 package Controleur;
-import java.util.Vector;
-public class Resultat {
+import android.widget.CheckBox;
 
-    char tabReponses[] = new char[6];
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Vector;
+public class Resultat implements Serializable
+{
+
+    ArrayList <CheckBox> list_result_cb;
+
+    public void add(CheckBox cb)
+    {
+        this.list_result_cb.add(cb);
+    }
 
 }
