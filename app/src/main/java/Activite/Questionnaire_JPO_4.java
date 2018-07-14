fd1d729f25763f06663f045b2594314bd4e26d3a
Kilingerx <38787801+Kilingerx@users.noreply.github.com>
2018-07-14 16:14:56 +0200

    Régu

--- app/src/main/java/Activite/Questionnaire_JPO_4.java
+++ app/src/main/java/Activite/Questionnaire_JPO_4.java
@@ -22,15 +22,4 @@ public class Questionnaire_JPO_4 extends AppCompatActivity {
         startActivity(new Intent(this, Questionnaire_JPO_4.class));
     }
 
-    {
-        //get the spinner from the xml.
-        Spinner dropdown = findViewById(R.id.spinner1);
-        //create a list of items for the spinner.
-        String[] items = new String[]{"1", "2", "three"};
-        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
-        //There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        //set the spinners adapter to the previously created one.
-        dropdown.setAdapter(adapter);
-    }
 }
